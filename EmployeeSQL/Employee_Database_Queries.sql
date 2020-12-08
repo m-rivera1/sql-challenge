13ad5bdcf8dd4b01504bd7b7c1cce40e4d55a8cf
m-rivera1 <mike.11.mxr@gmail.com>
2020-12-08 16:31:58 -0600

    completed query 8

--- EmployeeSQL/Employee_Database_Queries.sql
+++ EmployeeSQL/Employee_Database_Queries.sql
@@ -89,6 +89,13 @@ INNER JOIN dept_emp ON
 	OR dept_name LIKE 'Development';
 
 	
+--Query 8 - In descending order, list the frequency count of employee last names, i.e., 
+--           how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) AS "Frequency"
+FROM employees
+GROUP BY (last_name)
+ORDER BY (last_name) DESC;
 
 
 
