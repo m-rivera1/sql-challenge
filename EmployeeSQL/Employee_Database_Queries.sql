b95e03bd91ddc94af02f33b03002429d8802fe17
m-rivera1 <mike.11.mxr@gmail.com>
2020-12-07 23:15:03 -0600

    finished query 2

--- EmployeeSQL/Employee_Database_Queries.sql
+++ EmployeeSQL/Employee_Database_Queries.sql
@@ -11,14 +11,12 @@ INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
 
 --Query 2 - List first name, last name, and hire date for employees who 
---           were hired in 1986. 
+--           were hired in 1986..will use a range..could not figure how to
+--           convert to VARCHAR. 
 			
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE 
+WHERE hire_date >= '1986-01-01'
+AND hire_date < '1987-01-01'
 
 
-SELECT emp_no, last_name, first_name, sex
-FROM employees;
-SELECT salary
-FROM salaries;
